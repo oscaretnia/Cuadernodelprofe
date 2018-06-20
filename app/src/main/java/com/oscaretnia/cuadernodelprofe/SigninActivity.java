4ab00866967156014bf1ca29abfe0eb853dce275
Oscar Sepúlveda Pérez <oscaretnia.os@gmail.com>
2018-06-20 12:06:42 -0500

    Firebase Autenticacion añadida
    Firebase conectada en la SigninActivity

--- app/src/main/java/com/oscaretnia/cuadernodelprofe/SigninActivity.java
+++ app/src/main/java/com/oscaretnia/cuadernodelprofe/SigninActivity.java
@@ -1,10 +1,20 @@
 package com.oscaretnia.cuadernodelprofe;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +24,12 @@ public class SigninActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_signup_action) Button btnSignupAction;
     @BindView(R.id.btn_signin) Button btnSignin;
+    @BindView(R.id.email_field_signin) EditText emailField;
+    @BindView(R.id.password_field_signin) EditText passwordField;
+
+    private FirebaseAuth mAuth;
+
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,8 @@ public class SigninActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -36,7 +54,13 @@ public class SigninActivity extends AppCompatActivity {
                 break;
 
             case R.id.btn_signin:
-                signin();
+                email = emailField.getText().toString().trim();
+                password = passwordField.getText().toString().trim();
+
+                if(email.isEmpty() || password.isEmpty())
+                    showMessage("Campo vacío");
+                else
+                    signin(email, password);
                 break;
 
         }
@@ -44,10 +68,16 @@ public class SigninActivity extends AppCompatActivity {
     }
 
 
-    private void signin() {
+    private void signin(String email, String password) {
 
-        Intent intent = new Intent(SigninActivity.this, HomeActivity.class);
-        startActivity(intent);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) showMessage("Authentication success.");
+                        else showMessage("Authentication failed");
+                    }
+                });
         
     }
 
@@ -55,5 +85,13 @@ public class SigninActivity extends AppCompatActivity {
 
         Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
         startActivity(intent);
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(
+                getApplicationContext(),
+                message,
+                Toast.LENGTH_SHORT)
+                .show();
     }
 }
